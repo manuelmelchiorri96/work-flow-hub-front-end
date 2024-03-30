6860eb4c12c14272f23140ba3de8dc77e13874bd
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-03-30 11:52:21 +0100

    aggiunta layout component

---
+++ src/app/components/layout/layout.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayoutComponent } from './layout.component';
+import { RouterOutlet } from '@angular/router';
+
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutComponent],
+      imports: [RouterOutlet],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
