8db99998e4881a7580ddc28e10799c39a42d2218
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-03-30 11:54:18 +0100

    modifiche varie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    LayoutComponent,
+    RegistrazioneComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Dipendente } from '../models/dipendente';
+import { Credenziali } from '../models/credenziali';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private baseUrl: string =
+    'http://localhost:8080/Work_Flow_hub_Back_End_Jpa_Restful-1.0-SNAPSHOT/api/dipendenti';
+
+  constructor(private http: HttpClient) {}
+
+  login(credenziali: Credenziali): Observable<Dipendente> {
+    const url = `${this.baseUrl}/login`;
+    return this.http.post<Dipendente>(url, credenziali);
+  }
+
+  /*register(utente: UtenteRegister): Observable<Utente> {
+    const url = `${this.baseUrl}/register`;
+    return this.http.post<Utente>(url, utente);
+  }
+
+  update(utente: Utente): Observable<Utente> {
+    const url = `${this.baseUrl}/update`;
+    return this.http.put<Utente>(url, utente);
+  }
+
+  getUserInfo(idUtente: number): Observable<Utente> {
+    return this.http.get<Utente>(`${this.baseUrl}/${idUtente}`);
+  }
+
+  deleteAccount(idUtente: number) {
+    return this.http.delete(`${this.baseUrl}/${idUtente}`, {
+      responseType: 'text',
+    });
+  }*/
+}
