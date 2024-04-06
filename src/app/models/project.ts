b6ccfa0317a72340d0ae18448ff59bd886a1df0e
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-04-06 08:49:51 +0200

    aggiunti 2 campi nella classe project

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,26 +1,33 @@
 import { ProjectManager } from './projectManager';
+import { Task } from './task';
 
 export class Project {
+  idProgetto: number;
   nome: string;
   descrizione: string;
   dataInizio: string;
   dataFinePrevista: string;
   statoProgetto: string;
   projectManager: ProjectManager;
+  tasks: Task[];
 
   constructor(
+    idProgetto: number,
     nome: string,
     descrizione: string,
     dataInizio: string,
     dataFinePrevista: string,
     statoProgetto: string,
-    projectManager: ProjectManager
+    projectManager: ProjectManager,
+    tasks: Task[]
   ) {
+    this.idProgetto = idProgetto;
     this.nome = nome;
     this.descrizione = descrizione;
     this.dataInizio = dataInizio;
     this.dataFinePrevista = dataFinePrevista;
     this.statoProgetto = statoProgetto;
     this.projectManager = projectManager;
+    this.tasks = tasks;
   }
 }
